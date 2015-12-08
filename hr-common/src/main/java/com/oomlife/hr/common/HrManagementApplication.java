bdfc6e8dc381212860a7be6cf4ff1bf230126bc5
Yang Shuan <yangshuan45@gmail.com>
2015-12-08 23:05:34 +0800

    add main class of the application

---
+++ hr-common/src/main/java/com/oomlife/hr/common/HrManagementApplication.java
@@ -0,0 +1,15 @@
+package com.oomlife.hr.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main class of HR Management
+ * Created by yangshuan on 15/12/8.
+ */
+@SpringBootApplication
+public class HrManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HrManagementApplication.class, args);
+    }
+}
